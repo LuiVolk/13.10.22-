c951e1e1110d5f4fceae8244d6738b18902155b5
«Lui_Vo» <masy_@mail.ru>
2022-10-18 12:21:58 +0300

    task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿//Напишите программу, 
+//которая будет выдавать название дня недели по заданному номеру.
+//Пример
+//3 -> Среда
+//5 -> Пятница
+
+Console.WriteLine("Введите, пожалуйста, число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number == 1) Console.WriteLine("понедельник");
+else if (number == 2) Console.WriteLine("вторник");
+else if (number == 3) Console.WriteLine("среда");
+else if (number == 4) Console.WriteLine("четверг");
+else if (number == 5) Console.WriteLine("пятница");
+else if (number == 6) Console.WriteLine("суббота");
+else if (number == 7) Console.WriteLine("воскресенье");
+
+else Console.WriteLine("В неделе 7 дней, введите, пожалуйста, корректное число:");
+
+
+
+
+
+
