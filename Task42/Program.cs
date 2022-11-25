5c494d21cfb8b3ef6568a29334e37db2cc4bcff3
«Lui_Vo» <masy_@mail.ru>
2022-11-25 21:54:15 +0300

    add E[ampleLessonRecurs, Task39, Task42, Task44, Task45

---
+++ Task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine(" введите число в десятичной системе");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Binare(int num)
+{
+string st = "";
+while (num > 0)
+{
+string temp = Convert.ToString(num % 2);
+st = temp + st;
+num = num / 2;
+}
+
+int bin = Convert.ToInt32(st);
+return bin;
+}
+System.Console.WriteLine($"Число {number} в двоичной системе выглядит {Binare(number)}");
