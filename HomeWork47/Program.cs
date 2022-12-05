d412e41a2418e756d6dde194eaa6a089234f65b0
«Lui_Vo» <masy_@mail.ru>
2022-12-05 08:42:11 +0300

    HomeWork47

---
+++ HomeWork47/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateArrayDbl(int row, int column)
+{
+    double[,] array = new double[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+            if (new Random().Next(0, column) == j)
+            {
+                array[i, j] *= -1;
+            }
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+double[,] array = CreateArrayDbl(3, 4);
+PrintArray(array);
+
+
+
