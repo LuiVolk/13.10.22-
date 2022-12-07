e06866db7f6924ed327b7b2411b2e2d8e9b0587c
«Lui_Vo» <masy_@mail.ru>
2022-12-07 09:02:51 +0300

    HomeWork62

---
+++ HomeWork62/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write("\t{0:d2} ", array[i, j]);
+
+        Console.WriteLine(" ");
+    }
+    Console.WriteLine();
+}
+
+void FillSpiralMatrix(int[,] array)
+{
+    FillElementRec(0, 0, 1, 1, array);
+}
+
+void FillElementRec(int row, int column, int dir, int counter, int[,] matrix)
+{
+    if (matrix[row, column] == 0) matrix[row, column] = counter++;
+
+    if (dir == 1 && column < matrix.GetLength(1) - 1 && matrix[row, column + 1] == 0)
+        column++;
+    else if (dir == 2 && row < matrix.GetLength(0) - 1 && matrix[row + 1, column] == 0)
+        row++;
+    else if (dir == 3 && column > 0 && matrix[row, column - 1] == 0)
+        column--;
+    else if (dir == 0 && row > 0 && matrix[row - 1, column] == 0)
+        row--;
+    else
+    {
+        dir++;
+        dir %= 4;
+        if (dir == 1 && column < matrix.GetLength(1) - 1 && matrix[row, column + 1] == 0)
+            column++;
+        else if (dir == 2 && row < matrix.GetLength(0) - 1 && matrix[row + 1, column] == 0)
+            row++;
+        else if (dir == 3 && column > 0 && matrix[row, column - 1] == 0)
+            column--;
+        else if (dir == 0 && row > 0 && matrix[row - 1, column] == 0)
+            row--;
+        else return;
+    }
+    FillElementRec(row, column, dir, counter, matrix);
+}
+
+int[,] matrix = new int[4, 4];
+FillSpiralMatrix(matrix);
+PrintMatrix(matrix);
